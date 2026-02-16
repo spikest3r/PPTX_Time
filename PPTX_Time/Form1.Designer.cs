bc7dc47eb88b43a800522f13439b05d4e5b292cf
Oleg Sheremeta <me@olehsheremeta.com>
2026-02-16 23:00:54 +0200

    Add project files.

---
+++ PPTX_Time/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PPTX_Time
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total editing time";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(231, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 148);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(17, 66);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 20);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show in hours";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 166);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(432, 151);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Presentation info";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(219, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 97);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(351, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Editing time: ";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(18, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 72);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Title: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 48);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 350);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(479, 397);
+            this.MinimumSize = new System.Drawing.Size(479, 397);
+            this.Name = "Form1";
+            this.Text = "PPTX Time";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PPTX_Time/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace PPTX_Time
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<PPT> presentations;
+        int totalMinutes;
+
+        private void UpdateList()
+        {
+            listBox1.Items.Clear();
+            foreach(var item in presentations)
+            {
+                listBox1.Items.Add(item.name);
+            }
+        }
+
+        private void CalculateTotalTime()
+        {
+            float time = 0.0f;
+            string scale = "minutes";
+            FormatTime(totalMinutes, out time, out scale, checkBox1.Checked);
+            label1.Text = string.Format("Total editing time:\n {0} {1}", time, scale);
+        }
+
+        private int ProcessFile(string file)
+        {
+            int minutesValue = 0;
+            try
+            {
+                using (ZipArchive archive = ZipFile.OpenRead(file))
+                {
+                    ZipArchiveEntry coreEntry = archive.GetEntry("docProps/app.xml");
+                    ZipArchiveEntry thumbnailEntry = archive.GetEntry("docProps/thumbnail.jpeg");
+                    if (coreEntry != null)
+                    {
+                        using (Stream stream = coreEntry.Open())
+                        {
+                            PPT ppt = new PPT();
+
+                            ppt.name = Path.GetFileNameWithoutExtension(file);
+
+                            XmlDocument xmlDoc = new XmlDocument();
+                            xmlDoc.Load(stream);
+
+                            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+                            nsmgr.AddNamespace("ep", "http://schemas.openxmlformats.org/officeDocument/2006/extended-properties");
+
+                            XmlNode totalTimeNode = xmlDoc.SelectSingleNode("//ep:TotalTime", nsmgr);
+                            if (totalTimeNode != null && int.TryParse(totalTimeNode.InnerText, out int minutes))
+                            {
+                                minutesValue = minutes;
+                                ppt.rawTime = minutes;
+                            }
+                            else
+                            {
+                                ppt.rawTime = -1;
+                            }
+
+                            if (thumbnailEntry != null)
+                            {
+                                using (Stream entryStream = thumbnailEntry.Open())
+                                using (MemoryStream ms = new MemoryStream())
+                                {
+                                    entryStream.CopyTo(ms);
+                                    ms.Position = 0;
+                                    try
+                                    {
+                                        using (var tmp = Image.FromStream(ms))
+                                        {
+                                            ppt.thumbnail = new Bitmap(tmp); // Clean copy
+                                        }
+                                    }
+                                    catch { ppt.thumbnail = null; }
+                                }
+                            } else
+                            {
+                                ppt.thumbnail = null;
+                            }
+
+                            presentations.Add(ppt);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Format("Failed to file\n{0}", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return minutesValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                totalMinutes = 0;
+
+                pictureBox1.Image = null;
+
+                foreach (var item in presentations)
+                {
+                    if (item.thumbnail == null) continue;
+                    item.thumbnail.Dispose();
+                }
+
+                presentations.Clear();
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                string folderPath = folderBrowserDialog1.SelectedPath;
+                foreach (string file in Directory.GetFiles(folderPath, "*.pptx"))
+                {
+                    totalMinutes += ProcessFile(file);
+                }
+
+                UpdateList();
+
+                CalculateTotalTime();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void FormatTime(int inTime, out float time, out string scale, bool forceHours = false)
+        {
+            string[] scales = { "minutes", "hours", "days" };
+            int scaleIndex = 0;
+            float output = inTime;
+            if((inTime >= 60 && scaleIndex == 0) || forceHours)
+            {
+                scaleIndex++;
+                output = inTime / 60.0f; // minutes to hours
+            }
+            if (output >= 24 && scaleIndex == 1 && !forceHours)
+            {
+                scaleIndex++;
+                output /= 24.0f; // hours to days
+            }
+            time = output;
+            scale = scales[scaleIndex];
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int newIndex = listBox1.SelectedIndex;
+            if (newIndex < 0)
+            {
+                label2.Text = "Name";
+                label3.Text = "Editing time";
+                button3.Enabled = false;
+                pictureBox1.Image = null;
+                return;
+            }
+
+            var presentation = presentations[newIndex];
+            
+            string scale = "minutes";
+            float time = 0.0f;
+            FormatTime(presentation.rawTime, out time, out scale, checkBox1.Checked);
+
+            label2.Text = string.Format("Name: {0}", presentation.name);
+            label3.Text = string.Format("Editing time: {0} {1}", time, scale);
+
+            button3.Enabled = true;
+
+            pictureBox1.Image = presentation.thumbnail;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            presentations = new List<PPT>();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog1.FileName;
+                int minutes = ProcessFile(path);
+
+                totalMinutes += minutes;
+                UpdateList();
+
+                CalculateTotalTime();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var presentation = presentations[listBox1.SelectedIndex];
+            presentation.thumbnail?.Dispose();
+            totalMinutes -= presentation.rawTime;
+            presentations.RemoveAt(listBox1.SelectedIndex);
+            listBox1.ClearSelected();
+            UpdateList();
+            CalculateTotalTime();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            CalculateTotalTime(); // recalculate to fit new params
+        }
+
+        private bool _closing = false;
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_closing) return;
+            _closing = true;
+
+            var img = pictureBox1.Image;
+            pictureBox1.Image = null;
+            img?.Dispose();
+
+            base.OnFormClosing(e);
+        }
+    }
+
+    public class PPT
+    {
+        public string name;
+        public int rawTime; // minutes
+        public Image thumbnail;
+    }
+}
